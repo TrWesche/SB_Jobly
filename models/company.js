6b942b7dd7114a727e7cc52aae770df20ce537ed
Trenton Wesche <TrWesche@live.com>
2020-09-14 18:17:16 -0500

    Company routes, validation, and model ready for test

--- models/company.js
+++ models/company.js
@@ -14,19 +14,19 @@ class Company {
             let searchValues = [];
 
             if (queryParams.hasOwnProperty("search")) {
-                searchTerms.push(`(handle ILIKE '%$${idx}%' OR name ILIKE '%$${idx}%')`);
-                searchValues.push(queryParams.search);
+                searchTerms.push(`(handle ILIKE $${idx} OR name ILIKE $${idx})`);
+                searchValues.push('%'+queryParams.search+'%');
                 idx += 1;
             }
 
             if (queryParams.hasOwnProperty("min_employees")) {
-                searchTerm.push(`num_employees > $${idx}`)
+                searchTerms.push(`num_employees > $${idx}`)
                 searchValues.push(queryParams.min_employees);
                 idx += 1;
             }
 
             if (queryParams.hasOwnProperty("max_employees")) {
-                searchTerm.push(`num_employees < $${idx}`)
+                searchTerms.push(`num_employees < $${idx}`)
                 searchValues.push(queryParams.max_employees);
                 idx += 1;
             }
@@ -47,12 +47,12 @@ class Company {
         }   
     }
 
-    static async new({handle, name, num_employees, description, logo_url}) {
+    static async new(companyParams) {
         const result = await db.query(`
             INSERT INTO companies (handle, name, num_employees, description, logo_url)
             VALUES ($1, $2, $3, $4, $5)
             RETURNING handle, name, num_employees, description, logo_url`,
-            [handle, name, num_employees, description, logo_url]);
+            [companyParams.handle, companyParams.name, companyParams.num_employees, companyParams.description, companyParams.logo_url]);
         return result.rows[0]
     }
 
@@ -74,10 +74,9 @@ class Company {
     static async delete(handle) {
         const result = await db.query(`
             DELETE FROM companies
-            WHERE handle = $1`,
+            WHERE handle = $1
+            RETURNING handle`,
             [handle]);
-        // Need to check what result will contain on this
-        console.log("Delete Company:", result)
         return result.rows[0];
     }
 }
--- routes/companies.js
+++ routes/companies.js
@@ -1,22 +1,110 @@
 const express = require("express");
 const ExpressError = require("../helpers/expressError");
 const Company = require("../models/company");
-
+const jsonschema = require("jsonschema");
+const searchSchema = require("../schemas/companySearch.json");
+const createSchema = require("../schemas/companyNew.json");
+const e = require("express");
 
 const router = new express.Router()
 
 
 router.get("/", async (req, res, next) => {
     try {
-        // Need to add search parameter validations
-        if (req.query.length) {
+        let companyData;
+        // Check for query params validity
+        if (Object.keys(req.query).length && jsonschema.validate(req.query, searchSchema).valid) {
+            // If min and max exist, validate that max > min
+            if (req.query.hasOwnProperty("min_employees") && req.query.hasOwnProperty("max_employees")) {
+                if (req.query.min_employees > req.query.max_employees) {
+                    throw new ExpressError("Search Parameters are Invalid", 400)
+                }
+            }
+            companyData = await Company.all(req.query);
+        } else {
+            companyData = await Company.all(null);
         }
-        const companyData = await Company.all(null);
-        console.log(companyData)
-        return {companies: companyData};
+
+        return res.json({companies: companyData});
     } catch (error) {
         return next(error)
     }  
+})
+
+router.post("/", async(req, res, next) => {
+    try {
+        const validate = jsonschema.validate(req.body, createSchema);
+
+        if (!validate.valid) {
+            //Collect all the errors in an array and throw
+            const listOfErrors = result.errors.map(e => e.stack);
+            throw new ExpressError(`Unable to create a new Company: ${listOfErrors}`, 400)
+        }
+
+        const companyData = await Company.new(req.body);
+        return res.json({company: companyData})
+    } catch (error) {
+        return next(error)
+    }
+})
+
+router.get("/:handle", async(req, res, next) => {
+    try {
+        const companyData = await Company.get(req.params.handle);
+
+        return res.json({company: companyData})
+    } catch (error) {
+        return next(error)
+    }
+})
+
+router.patch("/:handle", async(req, res, next) => {
+    try {
+        // Validate company handle
+        const oldData = await Company.get(req.params.handle);
+        if (Object.keys(oldData).length === 0) {
+            throw new ExpressError(`Unable to find target company to update`, 400)
+        }
+
+        // Validate request data
+        const validate = jsonschema.validate(req.body, createSchema);
+        if (!validate.valid) {
+            const listOfErrors = result.errors.map(e => e.stack);
+            throw new ExpressError(`Unable to update Company: ${listOfErrors}`, 400)
+        }
+
+        // Build update list for patch query
+        let itemsList = {};
+        const newKeys = Object.keys(req.body);
+        newKeys.map(key => {
+            if((req.body[key] && oldData[key]) && (req.body[key] != oldData[key])) {
+                itemsList[key] = req.body[key];
+            }
+        })
+        
+        // If no changes return original data
+        if(Object.keys(itemsList).length === 0) {
+            return res.json({company: oldData});
+        }
+
+        // Update the company data with the itemsList information
+        const newData = await Company.update(req.params.handle, itemsList);
+        return res.json({company: newData})
+    } catch (error) {
+        return next(error)
+    }
+})
+
+router.delete("/:handle", async(req, res, next) => {
+    try {
+        const companyHandle = await Company.delete(req.params.handle);
+        if (!companyHandle) {
+            throw new ExpressError(`Unable to delete company ${req.params.handle}, record not found`, 404)
+        }
+        return res.json({message: `Company ${companyHandle.handle} Deleted`});
+    } catch (error) {
+        return next(error)
+    }
 })
 
 module.exports = router
